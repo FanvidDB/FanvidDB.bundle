1ea9d3b0d3b18fd75b28ffd8f6e6284e697fafa0
Kathryn Lingel <kathryn@lingel.net>
2021-03-22 18:56:08 -0700

    Skeleton/proof-of-concept settings

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,28 +1,73 @@
-AGENT_CACHE_TIME = CACHE_1HOUR * 24
 API_KEY = ''
-
 
 def Start():
     Log.Info('Starting FanvidDB metadata agent!')
 
 
 def ValidatePrefs():
-    Log.Info('Validating FanvidDB metadata agent preferences!')
+    Log.Info('Validating metadata against preferences!')
     global API_KEY
     API_KEY = Prefs.get('fanviddb_api_key')
+    Log.Info('API Key')
+    Log.Info(API_KEY)
 
 
 class FanvidDBAgent(Agent.Movies):
     name = 'FanvidDB'
     primary_provider = True
-    fallback_agent = None
-    accepts_from = ['com.plexapp.agents.localmedia']
+    fallback_agent = False
+    contributes_to = None
+    accepts_from = None
     languages = [Locale.Language.NoLanguage]
 
-    def search(self, results, media, lang):
-        Log.Info(media)
-        Log.Info(media.__dict__)
-        results.Append(MetadataSearchResult(id=media.id, name=media.name, score=86, lang=lang))
+    def search(self, results, media, lang, manual=False):
+        Log.Info('asdf')
+        for a in dir(media):
+            if not a.startswith('_'):
+                Log.Info('%s: %s', a, getattr(media, a))
+        Log.Info('fdsa')
+        results.Append(MetadataSearchResult(
+            id='youtube-dl|{}|{}'.format(media.filename, media.openSubtitlesHash),
+            name=media.title,
+            year=None,
+            lang=lang,
+            score=100
+        ))
+
+        results.Sort('score', descending=True)
 
     def update(self, metadata, media, lang):
-        pass
+        metadata.rating = 4.2  # out of 10 lmao
+        metadata.content_rating = 'mature rating'
+        #metadata.art
+        #metadata.chapters
+        metadata.themes = ['theme a', 'theme b']
+        metadata.quotes = ['quote me once', 'quote me twice']
+        metadata.year = 2222
+        #metadata.duration
+        metadata.rating_count = 5
+        metadata.genres = ['genre a', 'genre b']
+        #metadata.title
+        metadata.tagline = 'tagline believe it'
+        metadata.content_rating_age = 55
+        #metadata.writers
+        #metadata.collections
+        metadata.trivia = 'sounds trivial to me'
+        #metadata.tags
+        #metadata.audience_rating_image
+        #metadata.rating_image
+        #metadata.producers = ['producer a', 'producer b']
+        metadata.audience_rating = 6.9
+        metadata.studio = 'Studio 60'
+        #metadata.posters
+        #metadata.countries
+        #metadata.roles
+        #metadata.originally_available_at
+        #metadata.title_sort
+        #metadata.original_title
+        metadata.summary = 'summarize but good'
+        #metadata.reviews
+        #metadata.directors
+        #metadata.extras
+        #metadata.banners
+        #metadata.similar
